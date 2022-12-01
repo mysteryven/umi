e5a5d6e4a5f95450a12750df10a9050f42e1e4d3
wenzhe <mysteryven@gmail.com>
2022-12-01 14:29:08 +0800

    feat(create-umi): add antd version Select

--- packages/create-umi/src/index.test.ts
+++ packages/create-umi/src/index.test.ts
@@ -1,6 +1,7 @@
 import { rimraf } from '@umijs/utils';
-import { existsSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
 import { join } from 'path';
+import { prompts } from '@umijs/utils';
 import runGenerator from './index';
 
 const fixtures = join(__dirname, '..', 'fixtures');
@@ -41,5 +42,58 @@ test('generate plugin', async () => {
     },
   });
   expect(existsSync(join(cwd, 'src', 'index.ts'))).toEqual(true);
+  rimraf.sync(cwd);
+});
+
+test('no antd version select if not max template', async () => {
+  prompts.inject(['app', 'pnpm', 'https://registry.npmjs.org/']);
+  const cwd = join(fixtures, 'antd');
+  await runGenerator({
+    cwd,
+    args: {
+      _: [],
+      $0: '',
+    },
+  });
+
+  expect(existsSync(join(cwd, 'src', 'pages', 'index.tsx'))).toEqual(true);
+  rimraf.sync(cwd);
+});
+
+test('generate AntDesign Pro with V4', async () => {
+  const antdVersion = '^4.20.7';
+  prompts.inject(['max', antdVersion, 'pnpm', 'https://registry.npmjs.org/']);
+  const cwd = join(fixtures, 'antd');
+  await runGenerator({
+    cwd,
+    args: {
+      _: [],
+      $0: '',
+    },
+  });
+
+  const pkgContent = JSON.parse(
+    readFileSync(join(cwd, 'package.json'), 'utf-8'),
+  );
+  expect(pkgContent.dependencies.antd).toBe(antdVersion);
+  rimraf.sync(cwd);
+});
+
+test('generate AntDesign Pro with V5', async () => {
+  const antdVersion = '^5.0.0';
+  prompts.inject(['max', antdVersion, 'pnpm', 'https://registry.npmjs.org/']);
+  const cwd = join(fixtures, 'antd');
+  await runGenerator({
+    cwd,
+    args: {
+      _: [],
+      $0: '',
+    },
+  });
+
+  const pkgContent = JSON.parse(
+    readFileSync(join(cwd, 'package.json'), 'utf-8'),
+  );
+  expect(pkgContent.dependencies.antd).toBe(antdVersion);
   rimraf.sync(cwd);
 });
